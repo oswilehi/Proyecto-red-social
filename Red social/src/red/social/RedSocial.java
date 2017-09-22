8c6e0cf1e33d841365c6ecca3692a107739c5e87
katherine Lorenzo <krle_10@hotmail.com>
2017-09-22 10:23:53 -0600

    Add Frames and some methods for write.

--- Red social/src/red/social/RedSocial.java
+++ Red social/src/red/social/RedSocial.java
@@ -4,18 +4,52 @@
  * and open the template in the editor.
  */
 package red.social;
+import static java.awt.image.ImageObserver.WIDTH;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import javax.swing.DefaultListModel;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
-/**
- *
- * @author Krle__000
- */
 public class RedSocial {
 
-    /**
+    
+    public static final String DIRECTORY = "C:\\MEIA"; //const
+    public static final String USER_FILE = "Usuario.txt";
+    /*
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+      
+        LoginController();
+         try{
+             File MEIA = new File(DIRECTORY); 
+            if(!MEIA.exists()){
+                MEIA.mkdir();
+            }
+         }catch(Exception e){
+         }
     }
+    
+    public static void LoginController(){
+        Login log = new Login();
+        log.setVisible(true);
+    }
+    
+    public static void RegisterController(){
+        Registrarse Register = new Registrarse();
+        Register.setVisible(true);
+    }
+    
+   public static File LoadOrCreateFile(String Path){
+     //PrintWriter writer = new PrintWriter(Path,"UTF-8");   
+   }
     
 }
