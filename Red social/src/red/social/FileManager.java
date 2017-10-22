158ac0632d28d8e4fd93324ee3f79f77d079d98b
katherine Lorenzo <krle_10@hotmail.com>
2017-10-22 08:23:16 -0600

    added Length variable

--- Red social/src/red/social/FileManager.java
+++ Red social/src/red/social/FileManager.java
@@ -34,6 +34,8 @@ public class FileManager
 
    public static final String GROUPS_FRIENDS_FILE = "grupo_amigos.txt";
    public static final String INDEX  = "indice_";
+   
+   public static final int Length = 260;
    public static final int UserLength = 260;
    public static final int FriendLength = 77;
    public static final int GroupLength = 130; 
