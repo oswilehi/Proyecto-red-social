3e161c8677a255e26466e8dfabc4c300a55ee5c8
jasgs <jasgs@DESKTOP-FT1HJ5N>
2017-10-24 19:55:48 -0600

    Corregido un error en GetAllOfKey para archivos secuenciales indizados

--- Red social/src/red/social/FileManager.java
+++ Red social/src/red/social/FileManager.java
@@ -1727,6 +1727,7 @@ class SecuencialIndizado
             }
             indexDescription.close();
             
+            if (first == 0) return null;
             RandomAccessFile indexFile = FileManager.OpenFile(FileManager.INDEX + path);
             indexFile.seek((first -1) *FileManager.Length);
             
